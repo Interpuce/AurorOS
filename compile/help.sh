dfc311bd6aa62f37afaf46fb337263a302764d07
Gorciu <gorciuyt@gmail.com>
2024-10-22 11:31:42 +0200

    COMPILATION: help.sh did

---
+++ compile/help.sh
@@ -0,0 +1,4 @@
+echo "AurorOS Makefile"
+echo "  make - builds OS"
+echo "  make help - shows this help"
+echo "  make install-ubuntu - installs required packages for Ubuntu (you need to manually install on other distro)"
