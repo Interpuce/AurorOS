e1a3c902d92ebc6bc33ce5e7077c88b87f12de9a
Gorciu <gorciuyt@gmail.com>
2024-10-22 18:59:29 +0200

    next /boot files

---
+++ boot/README.md
@@ -0,0 +1,6 @@
+# Boot files
+
+These files are important to boot AurorOS. They are like bootloader.
+
+> [!WARNING]
+> Touching this folder may cause AurorOS to not read kernel properly.
---
+++ boot/load.c
@@ -0,0 +1,9 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2024 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
