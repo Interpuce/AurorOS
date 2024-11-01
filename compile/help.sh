5e40987f5195901d7fbd8cdb2621a13bdc51072a
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2024-11-01 23:23:16 +0100

    delete compile/help.sh

--- compile/help.sh
+++
@@ -1,5 +0,0 @@
-echo "AurorOS Makefile"
-echo "  make - builds OS"
-echo "  make help - shows this help"
-echo "  make install-ubuntu - installs required packages for Ubuntu (you need to manually install on other distro)"
-echo "  make run - tries to run OS on Ubuntu"
