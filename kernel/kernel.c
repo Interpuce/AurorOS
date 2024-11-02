973824cdf26dce61d05fc6afad69ae6b1e3a4748
Gorciu <gorciuyt@gmail.com>
2024-11-02 08:58:52 +0100

    space fix

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -14,27 +14,25 @@
 
 void main()
 {
-       clear_console();
-       print("Welcome to aurorOS!\n", 0x07);
-       while (1)
-       {
-                print("\nroot@aurorOS > ", 0x05);
-                
-                string ch = read_string(0x07);
-                if(string_equal(ch,"ver"))
-                {
-                        print("\nAurorOS beta sigma alpha domino pizza skibidi toilet\n", 0x07);
-                }
-                else if(string_equal(ch,"clear"))
-                {
-                        clear_console();
-                }
-                else
-                {
-                        print("\nInvalid command!\n", 0x04);
-                }
-                
-                print("\n", 0x07);        
-       }
-        
+    clear_console();
+    print("Welcome to aurorOS!\n", 0x07);
+    while (1)
+    {
+        print("\nroot@aurorOS > ", 0x05);
+  
+        string ch = read_string(0x07);
+        if(string_equal(ch,"ver"))
+        {
+            print("\nAurorOS beta sigma alpha domino pizza skibidi toilet\n", 0x07);
+        }
+        else if(string_equal(ch,"clear"))
+        {
+            clear_console();
+        }
+        else
+        {
+            print("\nInvalid command!\n", 0x04);
+        }
+        print("\n", 0x07);        
+    }  
 }
