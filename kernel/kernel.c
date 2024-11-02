ed37c2099ce44649d1e2b1110297a8a3f84dd840
Gorciu <gorciuyt@gmail.com>
2024-11-02 09:32:28 +0100

    new commands system!

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -15,24 +15,18 @@
 void main()
 {
     clear_console();
-    print("Welcome to aurorOS!\n", 0x07);
+    print("Welcome to AurorOS!\n", 0x07);
     while (1)
     {
-        print("\nroot@aurorOS > ", 0x05);
+        print("\nroot@AurorOS > ", 0x05);
   
         string ch = read_string(0x07);
-        if(string_equal(ch,"ver"))
-        {
-            print("\nAurorOS beta sigma alpha domino pizza skibidi toilet\n", 0x07);
-        }
-        else if(string_equal(ch,"clear"))
-        {
-            clear_console();
-        }
-        else
-        {
-            print("\nInvalid command!\n", 0x04);
-        }
-        print("\n", 0x07);        
+        int count;
+        string* charray = split_by_spaces(ch, count);
+
+        if (string_equal(charray[0], 'ver')) {
+            print("AurorOS 1.0.0", 0x07);
+            print("This version is very unstable and may contain errors and bugs. Let us know on dsc.gg/Auror-OS", 0x04);
+        }    
     }  
 }
