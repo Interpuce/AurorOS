a1d43b400a0501dfb7139d987a661f953c69c195
Gorciu <gorciuyt@gmail.com>
2024-11-02 11:10:23 +0100

    watermark

---
+++ fs/fat32.c
@@ -0,0 +1,11 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2024 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
+
+#include <types.h>
