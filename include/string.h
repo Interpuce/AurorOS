5e78856496c7979d5735f86edb4573a574fa6516
Gorciu <gorciuyt@gmail.com>
2024-11-03 20:49:28 +0100

    string.h new split by spaces

--- include/string.h
+++ include/string.h
@@ -35,6 +35,34 @@ uint8 string_equal(string ch1,string ch2)
     return result;
 }
 
+#define MAX_WORDS 100  // if someone knows how to do this without max_words make a pull request
 
+string* split_by_spaces(string ch, uint16* count) {
+    static string result[MAX_WORDS];
+    uint16 index = 0;
+    uint16 i = 0;
+
+    if (ch[0] == '\0') {
+        result[0] = ch;
+        *count = 1;
+        return result;
+    }
+
+    result[index++] = ch;
+
+    while (ch[i] != '\0') {
+        if (ch[i] == ' ') {
+            ch[i] = '\0';
+
+            if (ch[i + 1] != '\0' && ch[i + 1] != ' ' && index < MAX_WORDS) {
+                result[index++] = &ch[i + 1];
+            }
+        }
+        i++;
+    }
+
+    *count = index;
+    return result;
+}
 
 #endif
