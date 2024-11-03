2e43f469fadb04d3700e0e961a574fabeb898b86
Gorciu <gorciuyt@gmail.com>
2024-11-03 20:44:09 +0100

    small spaces

--- include/msghandler.h
+++ include/msghandler.h
@@ -4,33 +4,33 @@
 
 void print_info(string msg)
 {
-    print("\n[   ", 0x0F);
+    print("\n[ ", 0x0F);
     print("info ", 0x01);
-    print("  ]  ", 0x0F);
+    print(" ] ", 0x0F);
     print(msg, 0x07);
 }
 
 void print_warn(string msg)
 {
-    print("\n[   ", 0x0F);
+    print("\n[ ", 0x0F);
     print("warning ", 0x0E);
-    print("  ]  ", 0x0F);
+    print(" ] ", 0x0F);
     print(msg, 0x07);
 }
 
 void print_error(string msg)
 {
-    print("\n[   ", 0x0F);
+    print("\n[ ", 0x0F);
     print("ERROR ", 0x04);
-    print("  ]  ", 0x0F);
+    print(" ] ", 0x0F);
     print(msg, 0x07);
 }
 
 void print_ok(string msg)
 {
-    print("\n[   ", 0x0F);
+    print("\n[ ", 0x0F);
     print("OK ", 0x0A);
-    print("  ]  ", 0x0F);
+    print(" ] ", 0x0F);
     print(msg, 0x07);
 }
 
@@ -42,8 +42,8 @@ void println(string msg)
 
 void print_custom(string msg, string tp, int8 msgcl, int8 tpcl, int8 brcl)
 {
-    print("\n[   ", brcl);
+    print("\n[ ", brcl);
     print(tp, tpcl);
-    print("  ]  ", brcl);
+    print(" ] ", brcl);
     print(msg, msgcl);
 }
