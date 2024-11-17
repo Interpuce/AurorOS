4d8aaeaf6f3e25ba468f6786fc6e427b3d8444ec
Gorciu <gorciuyt@gmail.com>
2024-11-17 14:31:43 +0100

    arch comment

--- boot/boot.asm
+++ boot/boot.asm
@@ -6,6 +6,9 @@
 ;  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
 ; -------------------------------------------------------------------------
 
+; The AurorOS works in 32-bit architecture and currently does not support
+; any other architecture
+
 bits 32
 section .text
         align 4
@@ -14,8 +17,8 @@ section .text
         dd - (0x1BADB002+0x00)
         
 global start
-extern kernel_load
+extern kernel_load ; Declaration of kernel_load in boot/load.c
 start:
         cli
         call kernel_load
-        hlt
+        hlt ; Halt the processor
