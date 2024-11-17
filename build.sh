a8beab7572a8eaa80a09e2465ad70636b9048c1e
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2024-11-17 11:11:58 +0100

    update and rename build.sh to build.py

--- build.sh
+++
@@ -1,5 +0,0 @@
-nasm -f elf32 boot/boot.asm -o bootloader.o
-gcc -m32 -c kernel/kernel.c -o kernel.o -ffreestanding -nostartfiles -Iinclude
-ld -m elf_i386 -T compile/linker.ld -o kernel.bin bootloader.o kernel.o 
-
-qemu-system-x86_64 -kernel kernel.bin
