6524ffdec390ecf8546d62a6f8fb17da3f10d6e8
Gorciu <gorciuyt@gmail.com>
2024-11-20 18:10:00 +0100

    strncmp

--- include/string.h
+++ include/string.h
@@ -35,4 +35,17 @@ uint8 string_equal(string ch1,string ch2)
     return result;
 }
 
+
+int strncmp(const char *str1, const char *str2, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        if (str1[i] != str2[i]) {
+            return (unsigned char)str1[i] - (unsigned char)str2[i];
+        }
+        if (str1[i] == '\0' || str2[i] == '\0') {
+            return 0;
+        }
+    }
+    return 0;
+}
+
 #endif
