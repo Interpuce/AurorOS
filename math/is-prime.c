ae5021332f745b7429f7d7f122517bd17e8b3511
Gorciu <gorciuyt@gmail.com>
2024-11-22 12:38:47 +0100

    is prime

---
+++ math/is-prime.c
@@ -0,0 +1,22 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2024 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
+
+#include <types.h>
+
+int is_prime(int n) {
+    if (n <= 1) return 0; 
+    if (n <= 3) return 1;
+    if (n % 2 == 0 || n % 3 == 0) return 0;
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return 0;
+    }
+    return 1;
+}
