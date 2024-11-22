6551a10bd3adc7ecce799e64dd401e602de52b01
Gorciu <gorciuyt@gmail.com>
2024-11-22 12:23:03 +0100

    beta state kernel panic

--- boot/load.c
+++ boot/load.c
@@ -8,10 +8,18 @@
  * -------------------------------------------------------------------------
  */
 
+#include <constants.h>
+#include <panic.h>
 extern void kernel_main(void); // declares the /kernel/kernel.c file
 
 // A main function which sets up everything and loads the kernel main function.
 void kernel_load() {
+    // Early AUROR_BETA_STATE check
+    if (AUROR_BETA_STATE < 0 || AUROR_BETA_STATE >= 3) {
+        kernel_panic("INCORRECT_BETA_STATE");
+        return;
+    }
+
     // ToDo: set up interrupts
     kernel_main();
 }
