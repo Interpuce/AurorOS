12e5aca2b1e3ba035c6619b14d849eff02085249
Gorciu <gorciuyt@gmail.com>
2024-11-22 13:28:49 +0100

    time managment - realtime clock & time reading attempt

---
+++ include/rtc.h
@@ -0,0 +1,19 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2024 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include <types.h>
+
+typedef struct {
+    uint8_t hours;
+    uint8_t minutes;
+    uint8_t seconds;
+} time_t;
