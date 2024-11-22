55b7bb72ac0e5924ab4a97daddea42679af84e53
Gorciu <gorciuyt@gmail.com>
2024-11-22 12:46:30 +0100

    math declaration

---
+++ include/declarations/math.h
@@ -0,0 +1,20 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2024 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
+
+// DECLARATION FILE
+// The source for these function can be found at:
+//   lib/math
+
+#pragma once
+
+// Prime numbers
+extern int is_prime(int n);
+extern int next_prime_number(int n);
+extern int previous_prime_number(int n);
