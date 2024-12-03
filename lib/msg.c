fc21785030f704dbc9a68e7c80468e89f52cf616
Gorciu <gorciuyt@gmail.com>
2024-12-03 20:35:43 +0100

    changes, changes, changes - part 2

---
+++ lib/msg.c
@@ -0,0 +1,33 @@
+#include <screen.h>
+#include <string.h>
+#include <msg.h>
+
+void print_error(const char *str) {
+    printstr("[  ", 0x07);
+    printstr("ERROR", 0x04);
+    printstr("  ] ", 0x07);
+    printstr(str, 0x07);
+    printstr("\n", 0x07);
+}
+
+void print_info(const char *str) {
+    printstr("[  ", 0x07);
+    printstr("INFO", 0x01);
+    printstr("  ] ", 0x07);
+    printstr(str, 0x07);
+    printstr("\n", 0x07);
+}
+
+void print_ok(const char *str) {
+    printstr("[  ", 0x07);
+    printstr("OK", 0x0B);
+    printstr("  ] ", 0x07);
+    printstr(str, 0x07);
+    printstr("\n", 0x07);
+}
+
+void print_custom(const char *str, uint8_t color, uint8_t scolor) {
+    printstr("[  ", scolor);
+    printstr(str, color);
+    printstr("  ] ", scolor);
+}
