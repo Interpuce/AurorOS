9049cd89173db098be4e31229962b595ff4af377
Gorciu <gorciuyt@gmail.com>
2024-12-04 16:52:35 +0100

    fix

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <screen.h>
 
-void printprefix(const char *user, const char *pcname) {
+void printprefix(const char* user, const char* pcname) {
     printstr(user, 0x0B);
     printstr("@", 0x0F);
     printstr(pcname, 0x02);
