bbe7cf635c4d07251c9214ee853ead523dfc855c
Gorciu <gorciuyt@gmail.com>
2024-12-09 20:27:19 +0100

    inw added

--- include/ports.h
+++ include/ports.h
@@ -14,6 +14,8 @@
 
 uint8_t inb(uint16_t port);
 
+uint8_t inw(uint16_t port);
+
 void outb(uint16_t port, uint8_t value);
 
 void outw(uint16_t port, uint16_t data);
--- kernel/ports.c
+++ kernel/ports.c
@@ -12,14 +12,20 @@
 
 uint8_t inb(uint16_t port) {
     uint8_t result;
-    __asm__ volatile ("inb %1, %0" : "=a" (result) : "Nd" (port));
+    asm volatile ("inb %1, %0" : "=a" (result) : "Nd" (port));
+    return result;
+}
+
+uint8_t inw(uint16_t port) {
+    uint8_t result;
+    asm volatile ("inw %1, %0" : "=a" (result) : "Nd" (port));
     return result;
 }
 
 void outb(uint16_t port, uint8_t value) {
-    __asm__ volatile ("outb %0, %1" : : "a" (value), "Nd" (port));
+    asm volatile ("outb %0, %1" : : "a" (value), "Nd" (port));
 }
 
 void outw(uint16_t port, uint16_t data) {
-    __asm__ volatile ("outw %0, %1" : : "a"(data), "Nd"(port));
+    asm volatile ("outw %0, %1" : : "a"(data), "Nd"(port));
 }
