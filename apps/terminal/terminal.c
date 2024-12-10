67d5f941a88a41a24f898cb814466745b90b22b3
Gorciu <gorciuyt@gmail.com>
2024-12-10 22:27:35 +0100

    terminal as app

---
+++ apps/terminal/terminal.c
@@ -0,0 +1,77 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2024 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
+
+#include <string.h>
+#include <msg.h>
+#include <constants.h>
+#include <string.h>
+#include <input.h>
+#include <asm/power.h>
+
+#include "commands/commands.h"
+
+void printprefix(const char* user, const char* pcname) {
+    print(" [ ", 0x07);
+    print(user, 0x01);
+    print("@", 0x0F);
+    print(pcname, 0x02);
+    print(" ]", 0x07);
+    print(" $ ", 0x0F);
+}
+
+int terminal_main() {
+    clearscreen();
+    print("\n", 0x07);
+
+    uint16_t beta_state = true;
+    string current_user = "root";
+
+    if (beta_state == true) {
+        print_warn("You are running early build of AurorOS!");
+    }
+
+    char buffer[128];
+    char *args[10];
+    while (1) {
+        printprefix(current_user, PC_NAME);
+        read_str(buffer, sizeof(buffer), 0, 0x07);
+
+        int arg_count = split_str(buffer, ' ', args, 10);
+
+        if (arg_count > 0) {
+            char farg[1024] = "";
+            for (int i = 1; i < arg_count; i++) {
+                strcat(farg, args[i]);
+                strcat(farg, " ");
+            }
+
+            if (streql(args[0], "ver")) {
+                print(AUROR_NAME, 0x07);
+                println(AUROR_VERSION, 0x07);
+            } else if (streql(args[0], "print")) {
+                println(farg ,0x07);
+            } else if (streql(args[0], "cowsay")) {
+                cowsay(farg);
+            } else if (streql(args[0], "map")) {
+                map();
+            } else if (streql(args[0], "clear")) {
+                clearscreen();
+            } else if (streql(args[0], "reboot")) {
+                reboot();
+            } else if (streql(args[0], "shutdown")) {
+                shutdown();
+            } else {
+                print_error("Invalid command!");
+            }
+        }
+    }
+
+    kernelpanic("KERNEL_MAIN_LOOP_EXITED");
+}
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -8,69 +8,10 @@
  * -------------------------------------------------------------------------
  */
 
-#include "kernel.h"
-
-#include <string.h>
-#include <msg.h>
-#include <panic.h>
-#include <types.h>
-#include <input.h>
-#include <constants.h>
-#include <asm/power.h>
-
-#include "commands/commands.h"
-
-extern void init_threads_system();
+extern void init_threads_system(); // from /exec/exec.c
+extern int terminal_main(); // from /apps/terminal/terminal.c
 
 void main() {
     // Initialize executables
     init_threads_system();
-
-    clearscreen();
-    print("\n", 0x07);
-
-    uint16_t beta_state = true;
-    string current_user = "root";
-
-    if (beta_state == true) {
-        print_warn("You are running early build of AurorOS!");
-    }
-
-    char buffer[128];
-    char *args[10];
-    while (1) {
-        printprefix(current_user, PC_NAME);
-        read_str(buffer, sizeof(buffer), 0, 0x07);
-
-        int arg_count = split_str(buffer, ' ', args, 10);
-
-        if (arg_count > 0) {
-            char farg[1024] = "";
-            for (int i = 1; i < arg_count; i++) {
-                strcat(farg, args[i]);
-                strcat(farg, " ");
-            }
-
-            if (streql(args[0], "ver")) {
-                print(AUROR_NAME, 0x07);
-                println(AUROR_VERSION, 0x07);
-            } else if (streql(args[0], "print")) {
-                println(farg ,0x07);
-            } else if (streql(args[0], "cowsay")) {
-                cowsay(farg);
-            } else if (streql(args[0], "map")) {
-                map();
-            } else if (streql(args[0], "clear")) {
-                clearscreen();
-            } else if (streql(args[0], "reboot")) {
-                reboot();
-            } else if (streql(args[0], "shutdown")) {
-                shutdown();
-            } else {
-                print_error("Invalid command!");
-            }
-        }
-    }
-
-    kernelpanic("KERNEL_MAIN_LOOP_EXITED");
 }
