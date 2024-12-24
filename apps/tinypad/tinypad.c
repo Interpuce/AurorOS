0a009f5cf2db898d8b82d6b85817d2e1a6a191b4
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2024-12-24 16:33:15 +0100

    Tinypad C file

---
+++ apps/tinypad/tinypad.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <screen.h>
+#include <msg.h>
+#include <types.h>
+#include <input.h>
+
+int tinypad_main(uint8_t color, uint8_t barcolor) {
+    clearscreen();
+    paintline(0, barcolor);
+    printct("Tinypad", barcolor);
+    println(" ", 0x07);
+    char buffer[1762];
+    char *args[10];
+    while (1) {
+        read_str(buffer, sizeof(buffer), 0, color);
+        if (streql(buffer, ".exit")) {
+            break;
+        }
+    }
+    return 0;
+}
