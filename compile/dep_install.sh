fbdde273a74729ee3418e9441bbf75aedacfcd9f
Rościsław Szymański <156237164+I-use-Arch-btw-00111@users.noreply.github.com>
2025-01-10 14:04:55 +0100

    [UPDATE] dep_install.sh: Introduce loop, add command for Fedora's DNF package manager
    The script will now loop if an invalid choice is made by user.
    Also, a command was added to make the script be useful for Fedora users.

--- compile/dep_install.sh
+++ compile/dep_install.sh
@@ -12,27 +12,38 @@ echo -e "\033[1mIf you don't want to proceed, press \033[31mCtrl-C\033[0m \033[1
 sleep 0.2
 echo -e "\n"
 
-echo -e "\033[1m[CHOICE]\033[0m Select your distribution (or base, in case of derivatives):"
-echo "[A]rch [D]ebian"
-sleep 0.2
-echo -e "\n"
+# Loop until a valid choice is made
+while true; do
+  echo -e "\033[1m[CHOICE]\033[0m Select your distribution (or base, in case of derivatives):"
+  echo "[A]rch [D]ebian [F]edora"
+  sleep 0.2
+  echo -e "\n"
 
-read -r distro
+  read -r distro
 
-# Install dependencies with package manager applicable for selected distro
-case "$distro" in
-  [Aa])
-    echo -e "\033[1m[NOTIFY]\033[0m Proceeding with pacman."
-    sudo --user=root pacman -Sy
-    sudo --user=root pacman -S make python gcc binutils nasm qemu
-    ;;
-  [Dd])
-    echo -e "\033[1m[NOTIFY]\033[0m Proceeding with APT."
-    sudo --user=root apt update
-    sudo --user=root apt install make python3 gcc binutils nasm qemu-system-x86_64
-    ;;
-  *)
-    echo -e "\033[1m[ERROR]\033[0m Invalid selection. Please select [A]rch or [D]ebian."
-    ;;
-esac
+  # Install dependencies with package manager applicable for selected distro
+  case "$distro" in
+    [Aa])
+      echo -e "\033[1m[NOTIFY]\033[0m Proceeding with pacman."
+      sudo --user=root pacman -Sy
+      sudo --user=root pacman -S make python gcc binutils nasm qemu
+      break
+      ;;
+    [Dd])
+      echo -e "\033[1m[NOTIFY]\033[0m Proceeding with APT."
+      sudo --user=root apt update
+      sudo --user=root apt install make python3 gcc binutils nasm qemu-system-x86_64
+      break
+      ;;
+      [Ff])
+      echo -e "\033[1m[NOTIFY]\033[0m Proceeding with DNF."
+      sudo --user=root dnf install make python3 gcc binutils nasm qemu
+      break
+      ;;
+    *)
+      echo -e "\033[1m[ERROR]\033[0m Invalid selection. Please try again and select [A]rch, [D]ebian, or [F]edora."
+      sleep 0.2
+      ;;
+  esac
+done
 # That's it.
