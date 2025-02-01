78734632866fcd3cb6202950187ff385ab3ee04c
Gorciu <gorciuyt@gmail.com>
2025-02-01 22:06:19 +0100

    big changes coming out

--- compile/build.sh
+++ compile/build.sh
@@ -21,8 +21,8 @@ if [ ${#dependencies_missing[@]} -gt 0 ]; then
 fi
 
 ROOT_DIR=$(pwd)
-ASM_FILE="$ROOT_DIR/boot/boot.asm"
-ASM_OBJECT="$ROOT_DIR/bin/boot.o"
+ASM_FILE="$ROOT_DIR/kernel/boot/boot.asm"
+ASM_OBJECT="$ROOT_DIR/kernel/bin/boot.o"
 LINKER_SCRIPT="$ROOT_DIR/compile/linker.ld"
 KERNEL_OUTPUT="$ROOT_DIR/kernel.bin"
 ISO_OUTPUT="$ROOT_DIR/AurorOS.iso"
@@ -33,12 +33,19 @@ OBJECT_DIR="$ROOT_DIR/bin"
 
 mkdir -p "$OBJECT_DIR"
 
-echo ".c -> .o"
+echo ".c -> .o [kernel-level]"
 OBJECT_FILES=()
-for SOURCE_FILE in $(find "$ROOT_DIR" -type f -name '*.c' ! -name '*.excluded.c'); do
+for SOURCE_FILE in $(find "$ROOT_DIR/kernel/" -type f -name '*.c' ! -name '*.excluded.c'); do
     OBJECT_FILE="$OBJECT_DIR/$(basename "${SOURCE_FILE%.c}.o")"
     OBJECT_FILES+=("$OBJECT_FILE")
-    gcc -Wall -Wextra -m32 -ffreestanding -nostartfiles -Iinclude -nostdlib -fno-stack-protector -c "$SOURCE_FILE" -o "$OBJECT_FILE"
+    gcc -Wall -Wextra -m32 -ffreestanding -nostartfiles -Ikernel/include -nostdlib -fno-stack-protector -c "$SOURCE_FILE" -o "$OBJECT_FILE"
+done
+
+echo ".c -> .o [user-level]"
+for SOURCE_FILE in $(find "$ROOT_DIR/apps/" -type f -name '*.c' ! -name '*.excluded.c'); do
+    OBJECT_FILE="$OBJECT_DIR/$(basename "${SOURCE_FILE%.c}.o")"
+    OBJECT_FILES+=("$OBJECT_FILE")
+    gcc -Wall -Wextra -m32 -ffreestanding -nostartfiles -Iapps/include -nostdlib  -c "$SOURCE_FILE" -o "$OBJECT_FILE"
 done
 
 echo "boot.asm -> boot.o"
