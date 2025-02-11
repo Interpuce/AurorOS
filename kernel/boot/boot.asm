4b0803c67adb589816369575e6d3a26c9bbaeb31
Gorciu <gorciuyt@gmail.com>
2025-02-11 21:08:35 +0100

    update dates

--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -1,6 +1,6 @@
 ; -------------------------------------------------------------------------
 ;                                   AurorOS
-; (c) 2022-2024 Interpuce
+; (c) 2022-2025 Interpuce
 ; 
 ; You should receive AurorOS license with this source code. If not - check:
 ;  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
