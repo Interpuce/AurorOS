0fc3af3bab34bfe2a03f2f14a68269469e3bad64
Lopezzz <siemamordowalona@int.pl>
2025-02-15 17:31:51 +0100

    i somehow forgot to commit snprinf

--- kernel/lib/printf.c
+++ kernel/lib/printf.c
@@ -1,4 +1,83 @@
 #include <msg.h>
+#include <types.h>
+
+int snprintf(char* buffer, size_t size, const char* format, ...) {
+    char* ptr = (char*)&format + sizeof(char*);
+    int written = 0;
+
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (written >= size - 1) break; 
+
+        if (format[i] == '%' && format[i + 1] == 'd') {
+            int num = *(int*)ptr;
+            ptr += sizeof(int);
+
+            char numStr[20];
+            int j = 0, negative = 0;
+            if (num < 0) {
+                negative = 1;
+                num = -num;
+            }
+
+            do {
+                numStr[j++] = '0' + (num % 10);
+                num /= 10;
+            } while (num > 0);
+            
+            if (negative) {
+                numStr[j++] = '-';
+            }
+
+            for (int k = 0; k < j / 2; k++) {
+                char temp = numStr[k];
+                numStr[k] = numStr[j - k - 1];
+                numStr[j - k - 1] = temp;
+            }
+
+            for (int k = 0; k < j && written < size - 1; k++) {
+                buffer[written++] = numStr[k];
+            }
+            i++;
+        } else if (format[i] == '%' && format[i + 1] == 'x') {
+            int num = *(int*)ptr;
+            ptr += sizeof(int);
+
+            char numStr[20];
+            int j = 0;
+            
+            do {
+                int digit = num % 16;
+                numStr[j++] = (digit < 10) ? '0' + digit : 'a' + (digit - 10);
+                num /= 16;
+            } while (num > 0);
+
+            for (int k = 0; k < j / 2; k++) {
+                char temp = numStr[k];
+                numStr[k] = numStr[j - k - 1];
+                numStr[j - k - 1] = temp;
+            }
+
+            for (int k = 0; k < j && written < size - 1; k++) {
+                buffer[written++] = numStr[k];
+            }
+            i++;
+        } else if (format[i] == '%' && format[i + 1] == 's') {
+            char* str = *(char**)ptr;
+            ptr += sizeof(char);
+
+            while (*str != '\0' && written < size - 1) {
+                buffer[written++] = *str++;
+            }
+            i++;
+        } else {
+            buffer[written++] = format[i];
+        }
+    }
+
+    buffer[written] = '\0';
+    
+    return written;
+}
 
 void printf(const char* format, ...) {
     char* ptr = (char*)&format + sizeof(char*);
