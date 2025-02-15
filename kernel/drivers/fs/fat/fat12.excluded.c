b90c510944f0ce20db0a7ec64507f4c8c7bcf800
Gorciu <gorciuyt@gmail.com>
2025-02-15 18:14:53 +0100

    fat boot sector

---
+++ kernel/drivers/fs/fat/fat12.excluded.c
@@ -0,0 +1,28 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2025 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
+
+#include <types.h>
+
+struct fat32_boot_sector {
+    uint8_t jump[3];
+    uint8_t OEM_name[8];
+    uint16_t bytes_per_sector;
+    uint8_t sectors_per_cluster;
+    uint16_t reserved_sectors;
+    uint8_t fat_count;
+    uint16_t root_entries; 
+    uint16_t total_sectors;
+    uint8_t media_type; 
+    uint16_t fat_size;
+    uint16_t sectors_per_track;
+    uint16_t head_count;
+    uint32_t hidden_sectors;
+    uint32_t total_sectors_large;
+};
