61dc2c8e6864e5a0dc84b116faa52a7f4244c8dd
Gorciu <gorciuyt@gmail.com>
2025-03-16 12:52:48 +0100

    rewrite v2 - old ata (legacy, no atapi)

--- kernel/drivers/fs/disk.c
+++ kernel/drivers/fs/disk.c
@@ -92,3 +92,22 @@ int atapi_disk_read_sector(uint16_t base_port, uint8_t drive, uint32_t lba, uint
 
     return 0;
 }
+
+int ata_disk_read_sector(uint16_t base_port, uint8_t drive, uint32_t lba, uint8_t *buffer) {
+    outb(base_port + 6, 0x40 | (drive << 4) | ((lba >> 24) & 0x0F));
+    outb(base_port + 2, 1);
+    outb(base_port + 3, (uint8_t)(lba & 0xFF));
+    outb(base_port + 4, (uint8_t)((lba >> 8) & 0xFF));
+    outb(base_port + 5, (uint8_t)((lba >> 16) & 0xFF));
+    outb(base_port + 7, 0x20);
+
+    while (inb(base_port + 7) & 0x80);
+
+    for (uint16_t i = 0; i < 256; i++) {
+        uint16_t data = inw(base_port);
+        *buffer++ = (uint8_t)(data & 0xFF);
+        *buffer++ = (uint8_t)(data >> 8);
+    }
+
+    return 0;
+}
