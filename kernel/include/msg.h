abcdcb1ab9b62fcbf1043e0c3a65d7c70965d97c
Gorciu <gorciuyt@gmail.com>
2025-03-17 16:18:53 +0100

    access to snprintf

--- kernel/include/msg.h
+++ kernel/include/msg.h
@@ -20,3 +20,4 @@ extern void print_custom(const string str, uint8_t color, uint8_t scolor);
 extern void print(const string str, uint8_t color);
 extern void println(const string str, uint8_t color);
 extern void printf(const string format, ...);
+extern int snprintf(string buffer, size_t size, const string format, ...);
