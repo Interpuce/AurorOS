3eba4541e07fb485e588a672987658d45aac58a5
Gorciu <gorciuyt@gmail.com>
2025-03-24 16:35:46 +0100

    multitasking structure

---
+++ kernel/exec/multitasking.h
@@ -0,0 +1,39 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2025 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include <types.h>
+
+enum ThreadPriority {
+    THREAD_PRIORITY_SYSPROCESS,    // highest value for system-critical threads
+    THREAD_PRIORITY_HIGH,          // high priority & focused window
+    THREAD_PRIORITY_NORMAL,        // for normal threads
+    THREAD_PRIORITY_LOW            // for threads that are basically useless, but needs to work
+};
+
+struct ThreadEmulatedRegisters {
+    uint32_t eax;
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t eip;
+};
+
+struct Thread {
+    uint32_t thread_id;
+    ThreadEmulatedRegisters registers;
+    ThreadPriority priority;
+    bool within_breakpoint;
+};
