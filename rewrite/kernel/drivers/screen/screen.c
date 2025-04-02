a251c761a25ef77a6fd20d09663fa82f2eb8c23c
Gorciu <gorciuyt@gmail.com>
2025-04-02 18:25:00 +0200

    well headers

---
+++ rewrite/kernel/drivers/screen/screen.c
@@ -0,0 +1,10 @@
+/**
+ * -------------------------------------------------------------------------
+ *                                   AurorOS
+ * (c) 2022-2025 Interpuce
+ * 
+ * You should receive AurorOS license with this source code. If not - check:
+ *  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+ * -------------------------------------------------------------------------
+ */
+
