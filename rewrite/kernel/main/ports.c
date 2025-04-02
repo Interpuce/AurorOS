6e8fe98709024097367501487319ca75306feaaf
Gorciu <gorciuyt@gmail.com>
2025-04-02 18:27:50 +0200

    ports driver v2?

--- rewrite/kernel/main/ports.c
+++ rewrite/kernel/main/ports.c
@@ -16,14 +16,14 @@ uint8_t inb(uint16_t port) {
     return result;
 }
 
+void outb(uint16_t port, uint8_t value) {
+    asm volatile ("outb %0, %1" : : "a" (value), "Nd" (port));
+}
+
 uint16_t inw(uint16_t port) {
     uint16_t result;
     asm volatile ("inw %1, %0" : "=a" (result) : "Nd" (port));
     return result;
-}
-
-void outb(uint16_t port, uint8_t value) {
-    asm volatile ("outb %0, %1" : : "a" (value), "Nd" (port));
 }
 
 void outw(uint16_t port, uint16_t data) {
