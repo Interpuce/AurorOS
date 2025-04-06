6225fc5a83440db354fe5075e8096770573fb095
Gorciu <gorciuyt@gmail.com>
2025-04-06 10:11:26 +0200

    start assembly function

---
+++ rewrite/kernel/init/start.asm
@@ -0,0 +1,33 @@
+; -------------------------------------------------------------------------
+;                                   AurorOS
+; (c) 2022-2025 Interpuce
+; 
+; You should receive AurorOS license with this source code. If not - check:
+;  https://github.com/Interpuce/AurorOS/blob/main/LICENSE.md
+; -------------------------------------------------------------------------
+
+; If you are editing this file on a Visual Studio Code, please download
+; extension for x86 assembly syntax, because Visual Studio Code don't
+; highlight Assembly language without additional extensions.
+
+; boot.asm configures everything (including Multiboot Header required by GRUB
+; and other bootloaders to see AurorOS) and loads the kernel main function
+; from /main/main.c
+
+bits 16
+
+section .multiboot
+    align 4
+    dd 0x1BADB002
+    dd 0x00
+    dd -(0x1BADB002 + 0x00)
+
+section .text
+
+extern main
+global start
+
+start:
+    cli 
+    call main
+    hlt
