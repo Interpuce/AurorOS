5e5a10ea95c599927039f7e0507505caba3fc722
Gorciu <gorciuyt@gmail.com>
2025-11-30 11:48:50 +0100

    bump version - auror public beta 6

--- include/constants.h
+++ include/constants.h
@@ -11,7 +11,7 @@
 #pragma once
 
 #define AUROR_NAME "AurorOS"
-#define AUROR_VERSION "Public Beta 4"
+#define AUROR_VERSION "Public Beta 6"
 #define PC_NAME "auror"
 #define PC_ARCH "x86"
 #define AUROR_BETA_STATE 2
