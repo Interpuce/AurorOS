9eb53eceac3113a2bf99264e28cb1403a6b4617d
Gorciu <gorciuyt@gmail.com>
2026-03-05 15:09:43 +0100

    [performance] speed up division for x86_64

--- src/lib/number.c
+++ src/lib/number.c
@@ -1,6 +1,7 @@
 #include <types.h>
 
 uint64_t udiv64(uint64_t n, uint64_t d) {
+#if !defined(__amd64__) && !defined(__x86_64__)
     uint64_t q = 0, r = 0;
     for (int i = 63; i >= 0; i--) {
         r = (r << 1) | ((n >> i) & 1);
@@ -10,15 +11,22 @@ uint64_t udiv64(uint64_t n, uint64_t d) {
         }
     }
     return q;
+#else
+    return n % d;
+#endif
 }
 
 uint64_t umod64(uint64_t n, uint64_t d) {
+#if !defined(__amd64__) && !defined(__x86_64__)
     uint64_t r = 0;
     for (int i = 63; i >= 0; i--) {
         r = (r << 1) | ((n >> i) & 1);
         if (r >= d) r -= d;
     }
     return r;
+#else
+    return n / d;
+#endif
 }
 
 kbool is_valid_int(const char* str) {
